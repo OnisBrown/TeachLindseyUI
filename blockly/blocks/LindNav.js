3d626a65092d4442a2707bab303a3cf75ed25f3d
OnisBrown <onis.Brown@hotmail.co.uk>
2019-07-12 16:01:02 +0100

    GoTo template working

--- blockly/blocks/LindNav.js
+++ blockly/blocks/LindNav.js
@@ -1,18 +1,19 @@
 Blockly.Blocks['Location'] = {
   init: function() {
     this.jsonInit({
-      "message0": 'Exibit %1',
+      "message0": '%1 Exibit',
       "args0": [
         {
           "type": "field_dropdown",
           "name": "location",
           "options": [
-            ["Viking ship", "Boat"],
+            ["Viking ship", "boat"],
             ["Roman Currency", "coins"],
             ["Ancient clay Bowls", "bowls"]
           ]
         }
       ],
+      "output": "String",
       "colour": 160,
       "tooltip": "Pick a location from the drop down menu",
     });
@@ -22,20 +23,18 @@ Blockly.Blocks['Location'] = {
 Blockly.Blocks['GoTo'] = {
   init: function() {
     this.jsonInit({
-      "message0": 'Exibit %1',
+      "message0": 'Go to %1',
       "args0": [
         {
-          "type": "field_dropdown",
+          "type": "input_value",
           "name": "location",
-          "options": [
-            ["Viking ship", "Boat"],
-            ["Roman Currency", "coins"],
-            ["Ancient clay Bowls", "bowls"]
-          ]
+          "check": "String"
         }
       ],
+      "nextStatement": null,
+      "previousStatement": null,
       "colour": 160,
-      "tooltip": "Pick a location from the drop down menu",
+      "tooltip": "Insert a Location Block",
     });
   }
 };
--- blockly/generators/javascript/Lindsey.js
+++ blockly/generators/javascript/Lindsey.js
@@ -1,11 +1,14 @@
 Blockly.JavaScript['Location'] = function(block) {
   //GoTo a location
   var choice = block.getFieldValue('location');
-  var code =
+  var code =choice;
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
 Blockly.JavaScript['GoTo'] = function(block) {
   //GoTo a location
-  var choice = block.getFieldValue('location');
-  var code =
+  var choice = Blockly.JavaScript.valueToCode(block,'location', Blockly.JavaScript.ORDER_ATOMIC) || "'failing to grab var'";
+  console.log(choice);
+  var code = "alert('" + choice + "')";
+  return code;
 };
