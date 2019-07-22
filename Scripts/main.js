a3b1d38dcb0a3b4863454bf7f093acb064b8c662
OnisBrown <onis.Brown@hotmail.co.uk>
2019-07-22 14:56:48 +0100

    restructuring

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,1 +1,37 @@
+var workspace
 
+function init(){
+  workspace = Blockly.inject('blocklyDiv',
+    {toolbox: document.getElementById('toolbox'),
+     grid:
+        {
+          spacing:15,
+          length:4,
+          colour:'#ccc',
+          snap:true
+        }
+  });
+
+  Blockly.JavaScript.addReservedWords('code'); //make code a reserved word
+}
+
+function codeViewer() { // viewer for the code produced
+  var code = Blockly.JavaScript.workspaceToCode(workspace);
+  document.getElementById('codeDiv').innerHTML = code;
+}
+
+// function codeParse(obj){
+// 	return Function(obj)
+// }
+
+function executeCode() {
+  window.LoopTrap = 100;
+  Blockly.JavaScript.INFINITE_LOOP_TRAP = 'if(--window.LoopTrap == 0) throw "Infinite loop.";\n';
+  var code = Blockly.JavaScript.workspaceToCode(workspace);
+  try{
+    eval(code);
+  }
+  catch (e){
+    alert(e);
+  }
+}
