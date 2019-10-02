c66e81ca63200b185fb1b786c509d3e49867e479
OnisBrown <onis.Brown@hotmail.co.uk>
2019-10-02 17:21:09 +0100

    changed block definitions to allow dynamically generated blocks

--- blockly/blocks/LindNav.js
+++ blockly/blocks/LindNav.js
@@ -1,6 +1,4 @@
-Blockly.Blocks['goTo'] = {
-  init: function() {
-    this.jsonInit({
+var goToJSON ={
       "message0": 'Go to %1',
       "args0": [
         {
@@ -12,14 +10,16 @@ Blockly.Blocks['goTo'] = {
       "nextStatement": null,
       "previousStatement": null,
       "style":"procedure_blocks",
-      "tooltip": "Insert a Location Block",
-    });
+      "tooltip": "Insert a Location Block"
+};
+
+Blockly.Blocks['goTo'] = {
+  init: function() {
+    this.jsonInit(goToJSON);
   }
 };
 
-Blockly.Blocks['goToDescribeWhile'] = {
-  init: function() {
-    this.jsonInit({
+goToDescribeWhileJSON = {
       "message0": 'Go to %1 and give the standard talk',
       "args0": [
         {
@@ -36,13 +36,15 @@ Blockly.Blocks['goToDescribeWhile'] = {
       "previousStatement": null,
       "style":"procedure_blocks",
       "tooltip": "Insert an exhibitLs Block to get the premade talk on that exhibit",
-    });
+    };
+
+Blockly.Blocks['goToDescribeWhile'] = {
+  init: function() {
+    this.jsonInit(goToDescribeWhileJSON);
   }
 };
 
-Blockly.Blocks['goToDescribe'] = {
-  init: function() {
-    this.jsonInit({
+goToDescribeJSON = {
       "message0": 'Go to %1 and give the standard talk',
       "args0": [
         {
@@ -55,102 +57,112 @@ Blockly.Blocks['goToDescribe'] = {
       "previousStatement": null,
       "style":"procedure_blocks",
       "tooltip": "Insert an exhibitLs Block to get the premade talk on that exhibit",
-    });
+    };
+
+Blockly.Blocks['goToDescribe'] = {
+  init: function() {
+    this.jsonInit(goToDescribeJSON);
   }
+};
+
+moveJSON = {
+  "message0": 'move x: %1 y: %2 z: %3 metres',
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "x",
+      "min": -10,
+      "max": 10
+    },
+    {
+      "type": "field_number",
+      "name": "y",
+      "min": -10,
+      "max": 10
+    },
+    {
+      "type": "field_number",
+      "name": "z",
+      "min": -10,
+      "max": 10
+    }
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "colour": 160,
+  "tooltip": "move Lindsey x, y, z metres +- 10 metre per move",
 };
 
 Blockly.Blocks['move'] = {
   init: function() {
-    this.jsonInit({
-      "message0": 'move x: %1 y: %2 z: %3 metres',
-      "args0": [
-        {
-          "type": "field_number",
-          "name": "x",
-          "min": -10,
-          "max": 10
-        },
-        {
-          "type": "field_number",
-          "name": "y",
-          "min": -10,
-          "max": 10
-        },
-        {
-          "type": "field_number",
-          "name": "z",
-          "min": -10,
-          "max": 10
-        }
-      ],
-      "nextStatement": null,
-      "previousStatement": null,
-      "colour": 160,
-      "tooltip": "move Lindsey x, y, z metres +- 10 metre per move",
-    });
+    this.jsonInit(moveJSON);
   }
+};
+
+exhibitLsJSON = {
+  "message0": 'exhibit: %1',
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "exhibit",
+      "options": [
+        ["Bronze age barrow finds", "1.1"],
+        ["Gaius Valerius tombstone", "1.2"],
+        ["Claudia Crysis tombstone", "1.3"],
+        ["Reconstructed Early Medieval face", "1.4"],
+        ["Anglo-Scandinavian grave cover", "1.5"],
+        ["Medieval stone effigy", "1.6"],
+        ["Welton le Wold handaxes", "2.1"],
+        ["Neolithic polished axes", "2.2"],
+        ["Iron Age logboat", "2.3"],
+        ["Tattershall Thorpe grave assemblage", "2.4"],
+        ["Medieval cooking", "2.5"],
+        ["Fiskerton causeway", "3.1"],
+        ["Statuettes of Minerva", "3.2"],
+        ["Papal seal matrix", "3.3"],
+        ["Jewish face roof tile", "3.4"],
+        ["Witham Valley monasteries", "3.5"],
+        ["Bronze Age pot decoration", "4.1"],
+        ["Iron Age coins drill", "4.2"],
+        ["Early Medieval pendant", "4.3"],
+        ["Hanging Bowls", "4.4"],
+        ["Medieval alabaster", "4.5"]
+      ]
+    }
+  ],
+  "output": "String",
+  "style":"procedure_blocks",
+  "tooltip": "Pick an exhibit from the drop down menu",
 };
 
 Blockly.Blocks['exhibitLs'] = {
   init: function() {
-    this.jsonInit({
-      "message0": 'exhibit: %1',
-      "args0": [
-        {
-          "type": "field_dropdown",
-          "name": "exhibit",
-          "options": [
-            ["Bronze age barrow finds", "1.1"],
-            ["Gaius Valerius tombstone", "1.2"],
-            ["Claudia Crysis tombstone", "1.3"],
-            ["Reconstructed Early Medieval face", "1.4"],
-            ["Anglo-Scandinavian grave cover", "1.5"],
-            ["Medieval stone effigy", "1.6"],
-            ["Welton le Wold handaxes", "2.1"],
-            ["Neolithic polished axes", "2.2"],
-            ["Iron Age logboat", "2.3"],
-            ["Tattershall Thorpe grave assemblage", "2.4"],
-            ["Medieval cooking", "2.5"],
-            ["Fiskerton causeway", "3.1"],
-            ["Statuettes of Minerva", "3.2"],
-            ["Papal seal matrix", "3.3"],
-            ["Jewish face roof tile", "3.4"],
-            ["Witham Valley monasteries", "3.5"],
-            ["Bronze Age pot decoration", "4.1"],
-            ["Iron Age coins drill", "4.2"],
-            ["Early Medieval pendant", "4.3"],
-            ["Hanging Bowls", "4.4"],
-            ["Medieval alabaster", "4.5"]
-          ]
-        }
-      ],
-      "output": "String",
-      "style":"procedure_blocks",
-      "tooltip": "Pick an exhibit from the drop down menu",
-    });
+    this.jsonInit(exhibitLsJSON);
   }
+};
+
+startTourJSON = {
+  "message0": 'choose a stock tour: %1',
+  "args0": [
+    {
+      "type": "field_dropdown",
+      "name": "tour",
+      "options": [
+        ["Death and Burial tour", "death"],
+        ["Tools and technology tour", "tools"],
+        ["Religion and belief tour", "religion"],
+        ["Art and design tour", "art"]
+      ]
+    }
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "style":"procedure_blocks",
+  "tooltip": "Pick a tour from one of the pre-established ones",
 };
 
 Blockly.Blocks['startTour'] = {
   init: function() {
-    this.jsonInit({
-      "message0": 'choose a stock tour: %1',
-      "args0": [
-        {
-          "type": "field_dropdown",
-          "name": "tour",
-          "options": [
-            ["Death and Burial tour", "death"],
-            ["Tools and technology tour", "tools"],
-            ["Religion and belief tour", "religion"],
-            ["Art and design tour", "art"]
-          ]
-        }
-      ],
-      "nextStatement": null,
-      "previousStatement": null,
-      "style":"procedure_blocks",
-      "tooltip": "Pick a tour from one of the pre-established ones",
-    });
+    this.jsonInit(startTourJSON);
   }
 };
--- blockly/blocks/LindSpeech.js
+++ blockly/blocks/LindSpeech.js
@@ -1,57 +1,77 @@
+scriptJSON = {
+  "message0": 'place speech here %1',
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "script",
+    }
+  ],
+  "output": "String",
+  "colour": 160,
+  "tooltip": "Pick a location from the drop down menu"
+};
+
 Blockly.Blocks['script'] = {
   init: function() {
-    this.jsonInit({
-      "message0": 'place speech here %1',
-      "args0": [
-        {
-          "type": "field_input",
-          "name": "script",
-        }
-      ],
-      "output": "String",
-      "colour": 160,
-      "tooltip": "Pick a location from the drop down menu",
-    });
+    this.jsonInit(scriptJSON);
   }
 };
 
+giveSpeechJSON = {
+  "message0": 'say %1',
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "script",
+      "check": "String"
+    }
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "colour": 160,
+  "tooltip": "Insert a scrypt"
+};
 
 Blockly.Blocks['giveSpeech'] = {
   init: function() {
-    this.jsonInit({
-      "message0": 'say %1',
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "script",
-          "check": "String"
-        }
-      ],
-      "nextStatement": null,
-      "previousStatement": null,
-      "colour": 160,
-      "tooltip": "Insert a scrypt",
-    });
+    this.jsonInit(giveSpeechJSON);
   }
+};
+
+describeJSON = {
+  "message0": 'Give a standard talk on %1',
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "exhibit",
+      "check": "String"
+    }
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "style":"procedure_blocks",
+  "tooltip": "Insert an exhibitLs Block to get the premade talk on that exhibit"
 };
 
 Blockly.Blocks['describe'] = {
   init: function() {
-    this.jsonInit({
-      "message0": 'Give a standard talk on %1',
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "exhibit",
-          "check": "String"
-        }
-      ],
-      "nextStatement": null,
-      "previousStatement": null,
-      "style":"procedure_blocks",
-      "tooltip": "Insert an exhibitLs Block to get the premade talk on that exhibit",
-    });
+    this.jsonInit(describeJSON);
   }
+};
+
+askYNQuestionJSON = {
+  "message0": 'Ask a question %1',
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "question",
+      "check": "String"
+    }
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "style":"logic_blocks",
+  "tooltip": "type a question to ask"
 };
 
 Blockly.Blocks['askYNQuestion'] = {
@@ -68,7 +88,7 @@ Blockly.Blocks['askYNQuestion'] = {
       "nextStatement": null,
       "previousStatement": null,
       "style":"logic_blocks",
-      "tooltip": "type a question to ask",
+      "tooltip": "type a question to ask"
     });
   }
 };
