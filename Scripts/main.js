fc357adff1024447e3eb05c8ea5516f23f1e66d4
OnisBrown <Onis.Brown@hotmail.co.uk>
2019-10-25 02:55:44 +0100

    loggin distance every iteration

--- Scripts/main.js
+++ Scripts/main.js
@@ -182,6 +182,7 @@ function Picker(){
 					myTopic.subscribe(function(msg){
             var dist;
 						dist = personDist(msg.pose.position);
+            console.log(dist);
             if(dist < current[1] && dist >0){
               console.log("found person");
               Picker();
