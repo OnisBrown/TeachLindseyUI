6f281a3533a8e908339e9ca7cf7c690afd1f0ac7
OnisBrown <Onis.Brown@hotmail.co.uk>
2019-11-11 18:38:55 +0000

    Import botpress library and connect to chatbot

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,18 +2,17 @@ var workspace;
 var xml_txt;
 var commandQueue = new Array();
 var commandQueueL2 = new Array();
-window.botpressWebChat.init({ host: 'http://localhost:3000', botId: 'chatty_lindsey' })
+window.botpressWebChat.init({ host: 'http://10.5.42.157:3000', botId: 'chatty_lindsey' })
 var pivWork = new Worker('../webWorkers/pivPass.js');
 var gazeWork = new Worker('../webWorkers/gazePass.js');
 //var botStream = new Worker('../webWorkers/ChatSocket.js');
 var talking;
 var away;
+var pivAway;
 var gazeTargets = {
   people:[],
   objects:[]
 };
-
-function
 
 var startPos = {
   x: 0,
