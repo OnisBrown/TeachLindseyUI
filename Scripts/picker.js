711cd8b8882ca3ee44b29af2054d8ccd6c3aec43
OnisBrown <onis.Brown@hotmail.co.uk>
2020-02-25 14:10:05 +0000

    adjusted picker

--- Scripts/picker.js
+++ Scripts/picker.js
@@ -1,10 +1,9 @@
 function Picker(current){ // stack of commands from blocks
     return new Promise( resolve => {
-
       switch(current[0]){
     		case "waitPer":
           if(current[2]){
-            completion("this is simulation");
+            resolve("this is simulation");
           }
           else{
             personSense(current[1]);
@@ -26,7 +25,8 @@ function Picker(current){ // stack of commands from blocks
         case 'goToNode':
           var node = "WayPoint" + current[1];
           // console.log(node);
-          rwcActionGoToNode(node).on("result", function(status){console.log("goToWaypoint status: " + JSON.stringify(status));});
+          rwcActionGoToNode(node).on("result", (status)=>{
+			  resolve("goToWaypoint status: " + JSON.stringify(status));});
           break;
         case 'goToDesc':
           var node = dynDictExhibits[current[1]][0];
@@ -34,36 +34,53 @@ function Picker(current){ // stack of commands from blocks
           // console.log(curExhibitCoord);
           displayAction("going to exhibit: " + dynDictExhibits[current[1]][2]);
           perAction = true;
-          rwcActionGoToNode(node).on("result", function(status){
+          rwcActionGoToNode(node).on("result", (status)=>{
             console.log("gotoNodeDescGoTo: " + JSON.stringify(status));
             perAction = false;
             speechPrep(current[2], true);
             displayAction("describing exhibit: " + dynDictExhibits[current[1]][2]);
             perAction = true;
-            rwcActionDescribeExhibit(current[1]).on("result", function(){console.log("gotoNodeDescDesc: " + JSON.stringify(status)); talking = false; perAction = false;  setTimeout(function(){},2000)});
+            rwcActionDescribeExhibit(current[1]).on("result", ()=>{
+				talking = false; 
+				perAction = false;  
+				setTimeout(function(){
+					resolve("gotoNodeDescDesc: " + JSON.stringify(status));
+				},2000)
+					
+			});
           });
-
           break;
         case 'move':
           quatCalc(current[1][3]);
           console.log(qtn);
           displayAction("moving by \u2b06, \u27a1, \u27f3 :"+ current[1]);
-          rwcActionSetPoseRelative(current[1][0], current[1][1], current[1][2], qtn).on("result", function(status){console.log("move status: " + JSON.stringify(status)); perAction = false; });
+          rwcActionSetPoseRelative(current[1][0], current[1][1], current[1][2], qtn).on("result", (status)=>{
+			  console.log("move status: " + JSON.stringify(status));
+			  perAction = false; 
+			});
           break;
         case 'speech':
           speechPrep(current[2], false);
           displayAction("saying: " + current[1]);
-          rwcActionSay(current[1]).on("result", function(status){console.log("speech status: " + JSON.stringify(status)); talking = false;  setTimeout(function(){} ,2000)});
+          rwcActionSay(current[1]).on("result", (status)=>{
+			  talking = false;  
+			  setTimeout(function(){resolve("speech status: " + JSON.stringify(status));} ,2000)
+			});
           break;
         case 'desc':
           speechPrep(current[2], true);
           curExhibitCoord = dynDictExhibits[current[1]][1];
           console.log(curExhibitCoord);
           displayAction("describing exhibit: " + dynDictExhibits[current[1]][2]);
-          rwcActionDescribeExhibit(current[1]).on("result", function(){talking = false; setTimeout(function(){},2000)});
+          rwcActionDescribeExhibit(current[1]).on("result", ()=>{
+			  talking = false; 
+			  setTimeout(function(){resolve("exhibit descrbed")},2000)
+			});
           break;
         case 'startTour':
-          rwcActionStartTour(current[1]).on("result", function(){ });
+          rwcActionStartTour(current[1]).on("result", ()=>{
+			  resolve("tour finished");
+		  });
           break;
         case 'YNQ&A':
           //rwcActionSay(current[1]);
@@ -111,17 +128,20 @@ function Picker(current){ // stack of commands from blocks
 
           break;
         case 'gazeAtPosition':
-          rwcActionGazeAtPosition(current[1][0], current[1][1], current[1][2], current[1][3]).on("result", function(status){console.log("gazePos status: " + JSON.stringify(status)); });
+          rwcActionGazeAtPosition(current[1][0], current[1][1], current[1][2], current[1][3]).on("result", (status)=>{
+			  resolve("gazePos status: " + JSON.stringify(status)); 
+			});
           break;
         case 'gazeAtPerson':
           if(current[2]){
-            ;
+            resolve("this is the simulation");
           }
           else{
             displayAction("looking at nearest person for "+ current[1] + " seconds");
-            rwcActionGazeAtNearestPerson(current[1]+3).on("result", function(status){console.log("gazePer status: " + JSON.stringify(status)); ;});
+            rwcActionGazeAtNearestPerson(current[1]+3).on("result", (status)=>{
+				 ;});
             console.log((current[1]+5)*1000);
-            setTimeout(function(){},(current[1]+5)*1000);
+            setTimeout(function(){resolve("gazePer status: " + JSON.stringify(status));},(current[1]+5)*1000);
           }
           break;
         case 'waitTime':
