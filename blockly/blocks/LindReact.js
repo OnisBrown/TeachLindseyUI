e1ca813dd50cc62a1bcbd60d53667b88e84f95ee
OnisBrown <onis.Brown@hotmail.co.uk>
2020-02-25 15:10:34 +0000

    swapped React files to correct locations

--- blockly/blocks/LindReact.js
+++ blockly/blocks/LindReact.js
@@ -1,42 +1,86 @@
-Blockly.JavaScript['waitPerson'] = function(block) {
-  //GoTo a location
-  var dist = block.getFieldValue('distance');
-  var simBool = block.getFieldValue('simulator').toLowerCase();
-  var code = "waitPersonCode(" + dist + ","+ simBool +");\n";
-  return code;
+var waitPersonJSON ={
+  "message0": 'wait for a person to get within %1 metres and then: ',
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "distance",
+      "min": 1,
+      "max": 5
+    }
+  ],
+  "message1": '(is this the simulator? %1)',
+  "args1": [
+    {
+      "type": "field_checkbox",
+      "name":"simulator",
+      "checked": true
+    },
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "colour": "#FF7E47",
+  "tooltip": "waits for a person to get within distance before "
 };
 
-
-function waitPersonCode(dist, simBool)
-{
-	let result = await Picker(["waitPer", dist, simBool]);
-}
-
-Blockly.JavaScript['pause'] = function(block) {
-  //GoTo a location
-  var seconds = block.getFieldValue('time');
-  var code = "waitTimeCode(" + seconds +");\n";
-  return code;
+Blockly.Blocks['waitPerson'] = {
+  init: function() {
+    this.jsonInit(waitPersonJSON);
+  }
 };
 
-function waitTimeCode(pause)
-{
-	let result = await Picker(["waitTime", pause]);
-}
-
-Blockly.JavaScript['askOQuestion'] = function(block) {
-  //GoTo a location
-  var prompt = block.getFieldValue('prompt');
-  var code = `askOCode("${prompt}");\n`;
-  return code;
+var waitTimeJSON ={
+  "message0": 'wait for %1 seconds and and then: ',
+  "args0": [
+    {
+      "type": "field_number",
+      "name": "distance",
+      "min": 1,
+      "max": 5
+    }
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "colour": "#FF7E47",
+  "tooltip": "waits for an amount of seconds"
 };
 
-function askOCode(prompt)
-{
-	let result = await Picker(["askO", prompt]);
+Blockly.Blocks['pause'] = {
+  init: function() {
+    this.jsonInit(waitTimeJSON);
+  }
+};
+
+var askOQuestionJSON ={
+  "message0": 'Prompt for dialogue: %1',
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "prompt",
+      "check":"String"
+    }
+  ],
+  "nextStatement": null,
+  "previousStatement": null,
+  "style":"procedure_blocks",
+  "tooltip": "waits for a person to get within distance before "
+};
+
+Blockly.Blocks['askOQuestion'] = {
+  init: function() {
+    this.jsonInit(askOQuestionJSON);
+  }
+};
+
+var startJSON ={
+  "message0": 'Attach Blocks here',
+  "nextStatement": null,
+  "Hat": true,
+  "colour": "#4C7D4C",
+  "tooltip": "start here"
 }
 
-Blockly.JavaScript['start'] = function(block){
-  var code = '';
-  return code;
+Blockly.Blocks['start'] = {
+  init: function(){
+    this.jsonInit(startJSON);
+  }
 }
--- blockly/generators/javascript/LindReact.js
+++ blockly/generators/javascript/LindReact.js
@@ -1,86 +1,42 @@
-var waitPersonJSON ={
-  "message0": 'wait for a person to get within %1 metres and then: ',
-  "args0": [
-    {
-      "type": "field_number",
-      "name": "distance",
-      "min": 1,
-      "max": 5
-    }
-  ],
-  "message1": '(is this the simulator? %1)',
-  "args1": [
-    {
-      "type": "field_checkbox",
-      "name":"simulator",
-      "checked": true
-    },
-  ],
-  "nextStatement": null,
-  "previousStatement": null,
-  "colour": "#FF7E47",
-  "tooltip": "waits for a person to get within distance before "
+Blockly.JavaScript['waitPerson'] = function(block) {
+  //GoTo a location
+  var dist = block.getFieldValue('distance');
+  var simBool = block.getFieldValue('simulator').toLowerCase();
+  var code = "waitPersonCode(" + dist + ","+ simBool +");\n";
+  return code;
 };
 
-Blockly.Blocks['waitPerson'] = {
-  init: function() {
-    this.jsonInit(waitPersonJSON);
-  }
-};
 
-var waitTimeJSON ={
-  "message0": 'wait for %1 seconds and and then: ',
-  "args0": [
-    {
-      "type": "field_number",
-      "name": "distance",
-      "min": 1,
-      "max": 5
-    }
-  ],
-  "nextStatement": null,
-  "previousStatement": null,
-  "colour": "#FF7E47",
-  "tooltip": "waits for an amount of seconds"
-};
-
-Blockly.Blocks['pause'] = {
-  init: function() {
-    this.jsonInit(waitTimeJSON);
-  }
-};
-
-var askOQuestionJSON ={
-  "message0": 'Prompt for dialogue: %1',
-  "args0": [
-    {
-      "type": "field_input",
-      "name": "prompt",
-      "check":"String"
-    }
-  ],
-  "nextStatement": null,
-  "previousStatement": null,
-  "style":"procedure_blocks",
-  "tooltip": "waits for a person to get within distance before "
-};
-
-Blockly.Blocks['askOQuestion'] = {
-  init: function() {
-    this.jsonInit(askOQuestionJSON);
-  }
-};
-
-var startJSON ={
-  "message0": 'Attach Blocks here',
-  "nextStatement": null,
-  "Hat": true,
-  "colour": "#4C7D4C",
-  "tooltip": "start here"
+function waitPersonCode(dist, simBool)
+{
+	let result = await Picker(["waitPer", dist, simBool]);
 }
 
-Blockly.Blocks['start'] = {
-  init: function(){
-    this.jsonInit(startJSON);
-  }
+Blockly.JavaScript['pause'] = function(block) {
+  //GoTo a location
+  var seconds = block.getFieldValue('time');
+  var code = "waitTimeCode(" + seconds +");\n";
+  return code;
+};
+
+function waitTimeCode(pause)
+{
+	let result = await Picker(["waitTime", pause]);
+}
+
+Blockly.JavaScript['askOQuestion'] = function(block) {
+  //GoTo a location
+  var prompt = block.getFieldValue('prompt');
+  var code = `askOCode("${prompt}");\n`;
+  return code;
+};
+
+function askOCode(prompt)
+{
+	let result = await Picker(["askO", prompt]);
+}
+
+Blockly.JavaScript['start'] = function(block){
+  var code = '';
+  return code;
 }
