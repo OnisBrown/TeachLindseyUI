2b0c29b1f310c41b6c976b55483a205d68b00137
OnisBrown <onis.Brown@hotmail.co.uk>
2020-03-05 14:03:01 +0000

    adding express

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
-const http = require('http'),
-      url = require('url'),
+const express = require('express'),
 
-makeServer = function (request,response){
-   let path = url.parse(request.url).pathname;
-   console.log(path);
-   if(path === '/'){
-      response.writeHead(200,{'Content-Type':'text/html'});
-      response.write(index.html);
-   }
-   else if(path === '/about'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('About page');
-   }
-   else if(path === '/'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('Blog page');
-   }
-   else{
-     response.writeHead(404,{'Content-Type':'text/plain'});
-     response.write('Error page');
-   }
-   response.end();
- }
+server = express();
+server.set('port', process.env.PORT || 4000);
 
-server = http.createServer(makeServer);
+//server routes
+
+server.get('/', (request,response)=>{
+   response.send('Home page');
+});
+
+server.use((request,response)=>{
+   response.type('text/plain');
+   response.status(505);
+   response.send('Error page');
+});
 
 server.listen(4000,()=>{
-  console.log('Node server created on localhost:4000');
+  console.log('Express server created at port 4000');
 });
